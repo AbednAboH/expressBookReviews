268ef5fc7e421670fb8f2493df499d0a6726479c
abed <freazer122@gmail.com>
2025-04-08 11:00:47 -0400

    first 2 tasks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,18 +13,39 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const books_string=JSON.stringify(books,null,2);
+  return res.status(200).send(books_string);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn=req.params.isbn;
+  const book=books[isbn];
+  if (book){
+    res.status(200).json(book);
+  }
+  else {
+    res.status(404).json({mssage: "Book not found!"});
+  }
  });
-  
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
+  const author=req.params.author;
+  const booksByAuthor=[]
+  for (const book in books){
+    if (book.author===author){
+        booksByAuthor.push(book);
+        console.log(book);
+    }
+  }
+  if (books){
+    res.status(200).send(JSON.stringify(booksByAuthor,null,4));
+  }
+  else{
+    res.status(404).JSON({message: `Books by author ${author} not found`});
+  }
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
